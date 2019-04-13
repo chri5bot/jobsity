3098f1feec60caf5390f028f69224f99755208f3
Chris <chris.mood92@gmail.com>
2019-04-13 10:08:54 -0500

    Add basic routing, add home view and no match view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { GlobalStyle } from "./styles/reset.css";
+import Routes from "./routes";
 
 class App extends Component {
   render() {
     return (
       <div>
         <GlobalStyle />
-        <span>hey</span>
+        <Routes />
       </div>
     );
   }
---
+++ src/views/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div>hey Home</div>
+    </div>
+  );
+}
+
+export default Home;
