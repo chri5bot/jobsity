e28b60d1152e1f5da5cb6f96f03f5d034a6dc46b
Chris <chris.mood92@gmail.com>
2019-04-13 11:35:41 -0500

    Improve code, save events in localstorage

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -5,24 +5,29 @@ import moment from "moment";
 import "../../styles/react-big-calendar.css";
 import { HomeContainer } from "./style";
 
-// Setup the localizer by providing the moment (or globalize) Object
-// to the correct localizer.
-const localizer = BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+const localizer = BigCalendar.momentLocalizer(moment);
 
 function Home() {
-  const [events, setEvents] = useState([]);
+  const initialEvents = JSON.parse(
+    window.localStorage.getItem("events") || "[]"
+  );
+  const [events, setEvents] = useState(initialEvents);
 
   const handleSelectSlot = ({ start, end }) => {
     const title = window.prompt("New Event name");
     if (title) {
-      setEvents([
+      const newEvents = [
         ...events,
         {
           start,
           end,
           title
         }
-      ]);
+      ];
+
+      setEvents(newEvents);
+
+      window.localStorage.setItem("events", JSON.stringify(newEvents));
     }
   };
   return (
@@ -30,13 +35,14 @@ function Home() {
       <BigCalendar
         popup
         selectable
-        localizer={localizer}
         events={events}
+        localizer={localizer}
+        defaultDate={new Date()}
+        onSelectSlot={handleSelectSlot}
+        views={{ month: true, week: true }}
         defaultView={BigCalendar.Views.MONTH}
         scrollToTime={new Date(1970, 1, 1, 6)}
-        defaultDate={new Date()}
         onSelectEvent={event => alert(event.title)}
-        onSelectSlot={handleSelectSlot}
       />
     </HomeContainer>
   );
